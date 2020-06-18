c182085da97e884af4566302b7ec1754d0568ac7
adihd <47402232+adihd@users.noreply.github.com>
2020-06-18 10:58:30 +0300

    העתקה

---
+++ pic_script.js
@@ -0,0 +1,18 @@
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#imageUpload").change(function() {
+  document.getElementById("uploadpic").innerHTML = "Well done! You got 3 bonus points!!!";
+  readURL(this);
+    
+});
+
